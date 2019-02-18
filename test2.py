3fe281dd25d8f52ce8ce8844c75e0b279fe54c87
Samuel Tai <stimothy77@gmail.com>
2019-02-18 19:08:57 +0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -2,18 +2,35 @@ import numpy as np
 
 data = np.loadtxt("finefoods.txt.partaa",dtype = str,delimiter = '\n')
 
-dic = {}            
-temp = [None] * 3 ##
-user = ""
-Boo = 0
+dic = {}
+temp = [None] * 4 ##record transformation
+user = ""         ##userID
+boo = 0           ##if it is new user
+cnt = 0
 for line in data:
+	while cnt < 100:
+	    cnt += 1
+		if line[0:9] = "product/p":
+			if boo = 1:
+				dic[user] = [user,temp]
+			else:
+				dic[user] = dic[user]+[temp]
 
-	if line[0:9] = "product/p":
-		temp[0] = line[19:] 
-	elif line[0:9] = "review/us":
-		user = line[15:]
-		if user not in dic:
-			
-	
+			temp = [None] * 4
+			temp[0] = line[19:]
+		elif line[0:9] = "review/us":
+			user = line[15:]
+			if user not in dic:
+					boo = 1
+		elif line[0:9] = "review/he":
+			temp[1] = line[20:]
+		elif line[0:9] = "review/sc"
+			temp[2] = line[14:]
+		elif line[0:9] = "review/ti"
+			temp[3] = line[13:]
 
-  
+text_file = open("Outputtest.txt", "w")
+for u in dic:
+	text_file.write(dic[u])
+
+text_file.close()
