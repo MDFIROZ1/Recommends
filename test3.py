22b90f566fb79f86ec221c54d01846d1858c07f1
Samuel Tai <stimothy77@gmail.com>
2019-02-19 22:52:11 +0800

    Update test3.py
    Trying batch loading file

--- test3.py
+++ test3.py
@@ -1,44 +1,49 @@
 import numpy as np
 from operator import itemgetter
-
-
-data = np.loadtxt("finefoods.txt.partab",dtype = str,delimiter = '\n')
-
+path = ['finefoods.txt.partaa', 'finefoods.txt.partab', 'finefoods.txt.partac',
+ 'finefoods.txt.partad', 'finefoods.txt.partae', 'finefoods.txt.partaf',
+  'finefoods.txt.partag', 'finefoods.txt.partah', 'finefoods.txt.partai',
+   'finefoods.txt.partaj']
 dic = []
-user_list = []
-temp = [None] * 4 ##record transformation [productID,helpfulness,score,time]
-user = ""         ##userID
-boo = 0           ##if it is '1' then it's a new user
-index = None
-for line in data:
+for path in paths:
+	data = np.loadtxt(path,dtype = str,delimiter = '\n')
 
-	if line[0:9] == "product/p":
-		#print (temp)
-		if len(user) != 0:
-			for record in dic:
-				if record[0] == user:
-					record+=[temp]
-					boo = 1
-			if boo == 0:
-				dic.append([user,temp])
-			
-		temp = [None] * 4
-		temp[0] = line[19:]
-	elif line[0:9] == "review/us":
-		user = line[15:]
-	        '''
-		if user not in user_list:
-			boo = 1
-		else:
-			boo = 0
-		'''
-		
-	elif line[0:9] == "review/he":
-		temp[1] = line[20:]
-	elif line[0:9] == "review/sc":
-		temp[2] = line[14:]
-	elif line[0:9] == "review/ti":
-		temp[3] = line[13:]
+
+	##user_list = []
+	temp = [None] * 4 ##record transformation [productID,helpfulness,score,time]
+	user = ""         ##userID
+	boo = 0           ##if it is '1' then it's a new user
+	index = None
+	for line in data:
+
+		if line[0:9] == "product/p":
+			#print (temp)
+			if len(user) != 0:
+				for record in dic:
+					if record[0] == user:
+						record+=[temp]
+						boo = 1
+				if boo == 0:
+					dic.append([user,temp])
+
+			temp = [None] * 4
+			temp[0] = line[19:]
+		elif line[0:9] == "review/us":
+			user = line[15:]
+		        '''
+			if user not in user_list:
+				boo = 1
+			else:
+				boo = 0
+			'''
+
+		elif line[0:9] == "review/he":
+			temp[1] = line[20:]
+		elif line[0:9] == "review/sc":
+			temp[2] = line[14:]
+		elif line[0:9] == "review/ti":
+			temp[3] = line[13:]
+data = []
 
 ########sort the purchase record by time#####################
 
@@ -59,7 +64,7 @@ for rec in dic:
 
 text_file.close()
 
-		
+
 
 
 
